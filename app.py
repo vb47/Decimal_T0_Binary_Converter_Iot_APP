4013a98d0181297bea9e30bfce7508a99f517318
vb47 <64318916+vb47@users.noreply.github.com>
2020-07-11 14:01:03 +0530

    some improvements

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template
+from types import MethodType
+from flask import Flask, render_template, request
+
+
+def conv(x):
+    y = 0
+    while x!=0:
+        y = y*10 + x%2
+        x = x//2
+    return y
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    return render_template("main.html")
+
+@app.route("/result", methods = ['POST','GET'])
+def result():
+    dec = 0
+    if request.method == 'POST':
+        result = request.form
+        for key, value in result.items():
+            dec = value
+        
+        b = conv(int(dec))
+        return render_template("result.html", binary = b)
